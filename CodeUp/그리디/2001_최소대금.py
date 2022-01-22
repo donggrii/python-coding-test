6252d9b545aad0f84fb5f95d894a27c01f7b32b0
daniel <ld4230@gmail.com>
2022-01-22 18:38:48 +0900

    update codeup greedy 2001

--- CodeUp/그리디/2001_최소대금.py
+++ CodeUp/그리디/2001_최소대금.py
@@ -1,7 +1,17 @@
+# https://codeup.kr/problem.php?id=2001
+
 price = []
 for i in range(5):
     price.append(int(input()))
 pasta = price[:3]
 juice = price[3:]
 answer = (min(pasta) + min(juice)) * 1.1
-print(round(answer, 1))
+print(round(answer, 1))    # 987.8
+
+# 입력
+
+# 800
+# 700
+# 900
+# 198
+# 330

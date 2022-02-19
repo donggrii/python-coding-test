22620819e5b157cab7d3957ec8c1e2a84a20026b
daniel <ld4230@gmail.com>
2022-02-19 23:10:22 +0900

    modify codeup recursive 1901

--- CodeUp/재귀함수/1901_1부터 n까지 출력하기.py
+++ CodeUp/재귀함수/1901_1부터 n까지 출력하기.py
@@ -1,8 +1,9 @@
 # https://codeup.kr/problem.php?id=1901
 # 문제 : 정수 n이 주어질 때, 반복문을 사용하지 않고 1부터 정수 n까지 출력하는 재귀함수 설계하기
-n = int(input())
 
 # 풀이 1
+n = int(input())
+
 def recursive_1(num):
     if num != 1:
         recursive_1(num - 1)
@@ -11,6 +12,8 @@ def recursive_1(num):
 recursive_1(n)
 
 # 풀이 2
+n = int(input())
+
 def recursive_2(num, rec):
     if rec < num:
         print(rec)
@@ -20,3 +23,16 @@ def recursive_2(num, rec):
         
 print(recursive_2(n, 1))
 
+# Test Case.
+# 입력 : 10
+# < 출력 >
+# 1
+# 2
+# 3
+# 4
+# 5
+# 6
+# 7
+# 8
+# 9
+# 10

710dbfe2d8ba4410fae04967caa985ee52baa2ac
daniel <ld4230@gmail.com>
2022-07-13 18:07:14 +0900

    add mathematic solution

--- Programmers/Level 1/sum_between_two_integers.py
+++ Programmers/Level 1/sum_between_two_integers.py
@@ -19,6 +19,14 @@ def solution(a, b):
     return sum(range(a, b + 1))
 
 
+# 다른 풀이 3 : 수학 공식 (n x (n + 1) / 2) 변형 이용
+# 시간 복잡도 : O(1)
+# n -> (abs(a - b) + 1) : a ~ b 사이의 정수 개수
+# (n + 1) / 2 -> (a + b) / 2 : 처음과 끝 2개 정수씩 쌍을 이뤘을 때 모두 합이 같다는 걸 이용해서 다 더하고 2로 나누기
+def solution(a, b):
+    return (abs(a - b) + 1) * (a + b) / 2
+
+
 # Test Case 1.
 # a, b = 3, 5
 # return : 12

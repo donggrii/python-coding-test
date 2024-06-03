fb2081d16b9e47641353a1f3c87b82aa393d9a1e
donggrii <ld4230@gmail.com>
2024-06-03 16:26:23 +0900

    add: upload lv1 sql solutions

---
+++ Programmers/[SQL] Level 1/흉부외과 또는 일반외과 의사 목록 출력하기.sql
@@ -0,0 +1,11 @@
+-- SELECT - 흉부외과 또는 일반외과 의사 목록 출력하기 (Oracle)
+
+-- 내 풀이
+SELECT DR_NAME,
+       DR_ID,
+       MCDP_CD,
+       TO_CHAR(HIRE_YMD, 'YYYY-MM-DD') AS HIRE_YMD
+FROM DOCTOR
+WHERE MCDP_CD = 'CS' OR MCDP_CD = 'GS'
+-- WHERE MCDP_CD IN ('CS', 'GS')
+ORDER BY HIRE_YMD DESC, DR_NAME ASC;
